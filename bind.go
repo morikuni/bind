d05c1f8b3e1bded3e014ede88084e79aa76e11e5
morikuni <taihei.with+git@gmail.com>
2017-06-03 10:20:57 +0900

    Initial commit

---
+++ bind.go
@@ -0,0 +1,107 @@
+package bind
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"strconv"
+)
+
+type Getter interface {
+	Get(key string) string
+}
+
+type requestGetter struct {
+	*http.Request
+}
+
+func (g requestGetter) Get(key string) string {
+	return g.FormValue(key)
+}
+
+func FromRequest(r *http.Request, target interface{}) error {
+	return FromGetter(requestGetter{r}, target)
+}
+
+type mapGetter struct {
+	m map[string]string
+}
+
+func (g mapGetter) Get(key string) string {
+	return g.m[key]
+}
+
+func FromMap(m map[string]string, target interface{}) error {
+	return FromGetter(mapGetter{m}, target)
+}
+
+func FromGetter(getter Getter, target interface{}) error {
+	targetValue := reflect.ValueOf(target)
+	if targetValue.Kind() != reflect.Ptr || targetValue.IsNil() {
+		return errors.New("target is not pointer or nil")
+	}
+
+	targetType := reflect.TypeOf(target).Elem()
+	for i := 0; i < targetType.NumField(); i++ {
+		field := targetType.Field(i)
+		tag, ok := field.Tag.Lookup("bind")
+		if !ok {
+			tag = field.Name
+		}
+		value := getter.Get(tag)
+		err := assignValue(value, targetValue.Elem().Field(i))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func assignValue(value string, target reflect.Value) error {
+	if value == "" {
+		target.Set(reflect.Zero(target.Type()))
+		return nil
+	}
+
+	if target.Kind() == reflect.Ptr {
+		if target.IsNil() {
+			target.Set(reflect.New(target.Type().Elem()))
+		}
+		target = target.Elem()
+	}
+
+	switch target.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		n, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return err
+		}
+		target.SetInt(n)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		n, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return err
+		}
+		target.SetUint(n)
+	case reflect.Float32, reflect.Float64:
+		f, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return err
+		}
+		target.SetFloat(f)
+	case reflect.String:
+		target.SetString(value)
+	case reflect.Bool:
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return err
+		}
+		target.SetBool(b)
+	default:
+		return fmt.Errorf("cannot set to %s", target.Kind().String())
+	}
+
+	return nil
+}
